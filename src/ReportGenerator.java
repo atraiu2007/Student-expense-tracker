4759e99c63f17baeb2c7e499e645287a5857d524
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-09-18 01:23:57 +0530

    Update ReportGenerator.java

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 
+public class ReportGenerator {
+
+    public static void generateReport(ArrayList<Expense> expenses) {
+
+        double total = 0;
+
+        for (Expense expense : expenses) {
+            total += expense.getAmount();
+        }
+
+        System.out.println("\n===== REPORT =====");
+        System.out.println("Total Expenses: ₹" + total);
+        System.out.println("Number of Records: " + expenses.size());
+    }
+}
