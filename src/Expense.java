4d3975843315e55f8717e76fcf041f26577eba2b
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-09-18 01:22:41 +0530

    Update Expense.java

--- src/Expense.java
+++ src/Expense.java
@@ -1,1 +1,40 @@
+public class Expense implements Printable {
 
+    private int id;
+    private Category category;
+    private String description;
+    private double amount;
+
+    public Expense(int id, Category category, String description, double amount) {
+        this.id = id;
+        this.category = category;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println(id + " | " + category + " | " + description + " | ₹" + amount);
+    }
+
+    @Override
+    public String toString() {
+        return id + "," + category + "," + description + "," + amount;
+    }
+}
