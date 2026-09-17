80170892136c4a8fdb815c0bf6685c8fa02fdcc9
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-09-18 01:24:11 +0530

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,1 +1,12 @@
+public class Menu {
 
+    public static void showMenu() {
+
+        System.out.println("\n===== STUDENT EXPENSE TRACKER =====");
+        System.out.println("1. Add Expense");
+        System.out.println("2. View Expenses");
+        System.out.println("3. Generate Report");
+        System.out.println("4. Exit");
+        System.out.print("Enter choice: ");
+    }
+}
