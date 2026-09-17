ac5efb6b85ea87b215f692d67075e00668db0f5a
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-09-18 01:24:29 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,78 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ExpenseManager manager = new ExpenseManager();
+
+        while (true) {
+
+            Menu.showMenu();
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+
+                    System.out.println("Select Category:");
+                    System.out.println("FOOD");
+                    System.out.println("TRAVEL");
+                    System.out.println("EDUCATION");
+                    System.out.println("ENTERTAINMENT");
+                    System.out.println("OTHER");
+
+                    Category category =
+                            Category.valueOf(sc.next().toUpperCase());
+
+                    sc.nextLine();
+
+                    System.out.print("Description: ");
+                    String description = sc.nextLine();
+
+                    System.out.print("Amount: ");
+                    double amount = sc.nextDouble();
+
+                    if (InputValidator.isValidAmount(amount)) {
+
+                        manager.addExpense(
+                                new Expense(
+                                        id,
+                                        category,
+                                        description,
+                                        amount
+                                )
+                        );
+
+                        System.out.println("Expense added.");
+                    }
+
+                    break;
+
+                case 2:
+                    manager.viewExpenses();
+                    break;
+
+                case 3:
+                    ReportGenerator.generateReport(
+                            manager.getExpenses()
+                    );
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
