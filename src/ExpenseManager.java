7d2cccf2cc53bfeec162f01874074a702693cad5
atraiu2007 <atraiu.25bai11475@vitbhopal.ac.in>
2026-09-18 01:23:40 +0530

    Update ExpenseManager.java

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
 
+public class ExpenseManager {
+
+    private ArrayList<Expense> expenses;
+
+    public ExpenseManager() {
+        expenses = FileHandler.loadExpenses();
+    }
+
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+        FileHandler.saveExpenses(expenses);
+    }
+
+    public void viewExpenses() {
+
+        if (expenses.isEmpty()) {
+            System.out.println("No expenses available.");
+            return;
+        }
+
+        for (Expense expense : expenses) {
+            expense.printDetails();
+        }
+    }
+
+    public ArrayList<Expense> getExpenses() {
+        return expenses;
+    }
+}
